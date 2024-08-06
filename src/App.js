61a5572f228f60d227f1caf328f5c3f0256ca334
Sunil Dukare <sunil@devangles.com>
2024-08-06 19:17:35 +0530

    updated content

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Nhost!</h1>
+      <p>This is my simple React app hosted on Nhost.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
