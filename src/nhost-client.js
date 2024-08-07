6176a5788446c58cd7dd3ea190a5d9c9374d96e0
Sunil Dukare <sunil@devangles.com>
2024-08-07 11:29:33 +0530

    updated changes

--- src/nhost-client.js
+++
@@ -1,9 +0,0 @@
-import { NhostClient } from '@nhost/react';
-
-const nhost = new NhostClient({
-  subdomain: 'hrwnxbkjidebhmowwfrc', // Replace with your Nhost subdomain
-  region: 'ap-south-1', // Replace with your Nhost region
-});
-
-export default nhost;
-
