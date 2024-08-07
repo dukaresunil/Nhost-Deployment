2008e8ffe0c87bb7531a47eb2d4f7722b7e33b5e
Sunil Dukare <sunil@devangles.com>
2024-08-07 10:57:08 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { NhostProvider } from '@nhost/react';
+import { NhostClient } from '@nhost/react';
+
+// Replace with your Nhost backend URL
+const nhost = new NhostClient({
+  baseURL: 'https://your-backend-url.nhost.app',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NhostProvider client={nhost}>
+      <App />
+    </NhostProvider>
   </React.StrictMode>
 );
 
@@ -15,3 +24,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
